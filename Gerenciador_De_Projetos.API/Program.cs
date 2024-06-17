932a092a328b5f1393a98699276eaaa6c2189d09
Marco Ryan Marassi Marques <marcoryan2014@gmail.com>
2024-06-17 20:13:23 -0300

    ATT

--- Gerenciador_De_Projetos.API/Program.cs
+++ Gerenciador_De_Projetos.API/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
--- REACT/gerenciadordeprojetos/src/services/serviceMembros.js
+++ REACT/gerenciadordeprojetos/src/services/serviceMembros.js
@@ -1,9 +1,9 @@
 import Api from "../helpers/api"
 
 export async function GetMembros(){
-    return await Api.get("/Membros")
+    return await Api.get(`/Membros`)
 }
 
 export async function GetMembrosById(id){
-    
+    return await Api.get(`/Membros/GetMembrosById/${id}`)
 }
