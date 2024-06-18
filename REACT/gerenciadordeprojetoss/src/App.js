ebd09808b5b5277e50205d154994a298d3d5d389
Marco Ryan Marassi Marques <marcoryan2014@gmail.com>
2024-06-17 21:40:39 -0300

    att

---
+++ REACT/gerenciadordeprojetoss/src/App.js
@@ -0,0 +1,15 @@
+import React,{useState, useEffect} from "react";
+import { GetMembros } from "./services/serviceMembros.js";
+
+const App = () =>{
+  useEffect(() => {
+GetMembros().then(res => {console.log(res.data)})
+  },[])
+  
+  return (
+    <>
+    <h2>Olá Mundo!</h2>
+    </>
+  )
+}
+export default App;
--- REACT/gerenciadordeprojetoss/src/helpers/api.js
+++ REACT/gerenciadordeprojetoss/src/helpers/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 var urlBase = "http://localhost:5062/api"
     const Api = axios.create({
         baseUrl: urlBase
     })
 
-export default Api;
+    export default Api;
 
 // const Api = axios.create({
 //     baseURL: 'http://localhost:5062/api',
