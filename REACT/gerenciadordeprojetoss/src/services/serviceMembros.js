d625d8fa14a0ffa7a44dbec0dc6bd861e3d71f3a
Marco Ryan Marassi Marques <marcoryan2014@gmail.com>
2024-06-19 18:26:19 -0300

    Delete GerenciadorDeProjetoss - Projeto Teste

--- REACT/gerenciadordeprojetoss/src/services/serviceMembros.js
+++
@@ -1,9 +0,0 @@
-import Api from "../helpers/api"
-
-export async function GetMembros(){
-    return await Api.get(`/Membros`)
-}
-
-export async function GetMembrosById(id){
-    return await Api.get(`/Membros/GetMembrosById/${id}`)
-}
